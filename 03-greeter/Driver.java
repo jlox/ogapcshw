4200fa7882ec422ee8314f2bbda643906dd12980
jlox <altermuse3@gmail.com>
2014-09-22 08:00:15 -0400

    Update Driver.java

--- 03-greeter/Driver.java
+++ 03-greeter/Driver.java
@@ -4,10 +4,10 @@ import java.io.*;
 public class Driver {
     public static void main(String[] args){
 	String s="jeanne locker";
-	space = s.indexOf(" ");
-	lastname = s.substring(space);
-	firstname = s.substring(0, space);
-	return "last name is: " +  lastname;
-	return "first name is: " + firstname;
+	int space = s.indexOf(" ");
+	String lastname = s.substring(space);
+	String firstname = s.substring(0, space);
+	System.out.println("last name is: " +  lastname);
+	System.out.println("first name is: " + firstname);
     }
 }
