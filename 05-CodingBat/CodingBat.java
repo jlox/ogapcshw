ec208f0680d5346a61fe5c55968dbb9141c64b37
jlox <altermuse3@gmail.com>
2014-09-24 08:37:51 -0400

    classwork

--- 05-CodingBat/CodingBat.java
+++ 05-CodingBat/CodingBat.java
@@ -17,4 +17,43 @@ public int diff21(int n) {
   return (n-21) * 2;
   }
 }
+/* working */
+public boolean nearHundred(int n) {
+  if (Math.abs(100-n) <= 10) {
+  return true;
+  } else if (Math.abs(200-n) <= 10) {
+  return true;
+  } else {
+  return false;}
+}
+
+/* working */
+public int teaParty(int tea, int candy) {
+  if (tea < 5 || candy < 5) {
+  return 0;
+  } else if ((tea * 2) <= candy || (candy * 2) <= tea) {
+  return 2;
+  } else {
+  return 1;}
+}
+
+/* working */
+public boolean lastDigit(int a, int b, int c) {
+  if (a%10 == b%10 || a%10 == c%10 || b%10 == c%10){
+  return true;
+  } else {return false;}
+}
+
+/* working */
+public boolean mixStart(String str) {
+  if (str.length() < 3) {
+  return false;
+  }
+  String Jeanne = str.substring(1);
+  if (Jeanne.startsWith("ix")) {
+  return true;
+  } else {
+  return false;
+  }
+}
 }
--- 05-CodingBat/Driver.java
+++ 05-CodingBat/Driver.java
@@ -1,8 +1,14 @@
 public class Driver {
 public static void main (String[] args) {
     CodingBat homework = new CodingBat();
-    homework.nonStart("hello", "world");
-    homework.makeAbba("monkey", "butt");
-    homework.diff21(7);
+    System.out.println(homework.nonStart("hello", "world"));
+    System.out.println(homework.makeAbba("monkey", "butt"));
+    System.out.println(homework.diff21(7));
+    System.out.println(homework.nearHundred(96));
+    System.out.println(homework.nearHundred(89));
+    System.out.println(homework.teaParty(6, 13));
+    System.out.println(homework.lastDigit(7, 27, 6));
+    System.out.println(homework.mixStart("hixtoom"));
+
 }
 }
