fcc0938959ad80bacad52f2dd1ac853bddd7ce48
jlox <altermuse3@gmail.com>
2014-09-23 21:05:10 -0400

    confused with skeleton

---
+++ 05-CodingBat/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+public static void main (String[] args) {
+    CodingBat homework = new CodingBat();
+    homework.nonStart("hello", "world");
+    homework.makeAbba("monkey", "butt");
+    homework.diff21(7);
+}
+}
