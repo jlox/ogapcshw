3e57d794a1e0dfe2b5b93e29737c9744fd46ea20
jlox <altermuse3@gmail.com>
2014-10-01 08:42:47 -0400

    hello

---
+++ classwork/10.1/Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+    public static void main(String[] args){
+	box test = new box();
+	System.out.println(test.box(4, 3));
+	System.out.println(test.tri1(4));
+	System.out.println(test.tri2(4));
+    }
+}
+		      
