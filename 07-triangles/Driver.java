f8709b79cc76aaae9e542956e6b8b54795d2b858
jlox <altermuse3@gmail.com>
2014-10-02 22:13:11 -0400

    i am so confused

--- 07-triangles/Driver.java
+++ 07-triangles/Driver.java
@@ -4,6 +4,8 @@ public class Driver{
 	System.out.println(test.box(4, 3));
 	System.out.println(test.tri1(4));
 	System.out.println(test.tri2(4));
+	System.out.println(test.diamond(5));
+	System.out.println(test.tri4(5));
     }
 }
 		      
--- 07-triangles/box.java
+++ 07-triangles/box.java
@@ -55,5 +55,90 @@ public class box{
 	}
 	return triangle;
     }
-}
 
+    public String tri3(int h){
+	int width=0;
+	String s="";
+	/*int i=1;
+	  while (i<h){
+	      int blah=0;
+	      while (blah<h-i){
+		  s+=" ";
+		  blah++;
+	      }
+	      while (blah<h+i-1){
+		  s+="*";
+		  blah++;
+	      }
+	      s+="\n";
+	      i++;
+	      }*/
+	
+	for (int i=1;i<h;i++){
+	      for (int blah=0;blah<h-i;blah++){
+		  s+=" ";
+	      }
+	 
+	      for (int blah=h-i;blah<h+i-1;blah++){
+		  s+="*";
+	      }
+	      s+="\n";
+       }
+       return s;
+	
+    }
+   
+    public String diamond(int h){
+	String s= "";
+	int tall = 1;
+	int i = 1;
+	while (i<(h/2)+1){
+	      int blah=0;
+	      while (blah<h-i){
+		  s+=" ";
+		  blah++;
+	      }
+	      while (blah<h+i-1){
+		  s+="*";
+		  blah++;
+	      }
+	      s+="\n";
+	      i++;
+	}
+	while (i < h){
+	    int blah = h - 2;
+	    while (blah<h-1){
+		s+="*";
+		blah++;
+	    }
+	    while (blah<h+i-1){
+		s+=" ";
+		blah++;
+	    }
+	    s+="\n";
+	    i++;
+	}
+	return s;
+    }
+  
+    public class tri4(int h){
+	int totip = h;
+	String result = "";
+	int it = 0;
+	int fat = 0;
+	while (totip > 0){
+	    while (fat < h){
+		if (it > 0){
+		    result = result + " ";
+		} else {
+		    result = result + "*";
+		}
+		fat = fat + 1;
+	    }
+		totip = totip - 1;
+		it = it + 1;
+		result = result + "\n";
+	return result;
+    }
+}
+}
