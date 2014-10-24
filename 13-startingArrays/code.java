6a52791c6e263761c351bf3d5c3961a9c4c44b83
jlox <altermuse3@gmail.com>
2014-10-24 08:16:34 -0400

    updated

--- 13-startingArrays/code.java
+++ 13-startingArrays/code.java
@@ -1,22 +1,15 @@
-// returns error "Syntax error on token "return", invalid Type"
+public class code{
 public int[] frontPiece(int[] nums) {
   int[] result;
-  int count = 0;
   if (nums.length >= 2){
   result = new int[2];
   result[0] = nums[0];
   result[1] = nums[1];
-  }
-  if (nums.length == 1){
-  result = new int[1];
-  result[0] = nums[0];
   } else {
-  count = 1;
-  }
+  result = nums;
   }
   return result;
 }
-
 
 public int sum13(int[] nums) {
   int count = 0;
@@ -33,4 +26,5 @@ public int sum13(int[] nums) {
   }
   }
   return answer;
+}
 }
