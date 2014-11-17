57aa538534b56ea592be36e61980b1dff60225bd
Jeanne Locker <jeannelocker@earthlink.net>
2014-11-17 08:39:34 -0500

    classstuff

---
+++ 17-arraylist/Driver1.java
@@ -0,0 +1,5 @@
+public class Driver1{
+    public static void main(String[] args){
+	ArrayList<Integer> a1 = new ArrayList();
+	int[] a = new int[10];
+	
---
+++ 17-arraylist/Randomize1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+
+public class Randomize {
+    Random r = new Random();
+
+    // Using ArrayLists //
+    //
